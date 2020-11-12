fa9634cd093043046bb2fa23e0349b4e95d657db
unknown <1950212193@qq.com>
2020-11-12 16:43:05 +0800

    This is frist git

---
+++ app/src/main/java/com/example/recylcerviewactivity/Fruit.java
@@ -0,0 +1,24 @@
+package com.example.recylcerviewactivity;
+
+
+
+
+public  class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String fruitName, int fruitImage){
+        this.name = fruitName;
+        this.imageId = fruitImage;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
+
+
